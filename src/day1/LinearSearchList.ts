cc8c77e78b9a0b7385dd141a70ef079306b35d75
mgm <tommin231@gmail.com>
2023-11-03 21:33:55 +0900

    style: 4 space indent

--- src/day1/LinearSearchList.ts
+++ src/day1/LinearSearchList.ts
@@ -1,9 +1,12 @@
-export default function linear_search(haystack: number[], needle: number): boolean {
-  for (let i = 0; i < haystack.length; ++i) {
-    if(haystack[i] === needle) {
-      return true
+export default function linear_search(
+    haystack: number[],
+    needle: number,
+): boolean {
+    for (let i = 0; i < haystack.length; ++i) {
+        if (haystack[i] === needle) {
+            return true;
+        }
     }
-  }
 
-  return false
+    return false;
 }
